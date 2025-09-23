6a4a2951349166efbd96f030dee718351bca6bcc
hoanglamnguyentb <hoanglamnguyentb@gmail.com>
2025-09-23 20:12:17 +0700

    route default

--- Hinet.Web/App_Start/RouteConfig.cs
+++ Hinet.Web/App_Start/RouteConfig.cs
@@ -3,44 +3,38 @@ using System.Web.Routing;
 
 namespace Hinet.Web
 {
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            // BotDetect requests must not be routed
-            routes.IgnoreRoute("{*botdetect}",
-            new { botdetect = @"(.*)BotDetectCaptcha\.ashx" });
+	public class RouteConfig
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+			// BotDetect requests must not be routed
+			routes.IgnoreRoute("{*botdetect}",
+			new { botdetect = @"(.*)BotDetectCaptcha\.ashx" });
 
-            routes.MapRoute(
-              name: "CheckLogin",
-              url: "CheckLogin",
-              defaults: new { controller = "Dashboard", action = "GetInfoUserLogin", id = UrlParameter.Optional }
-                                ).DataTokens = new RouteValueDictionary(new { area = "DashboardArea" });
+			routes.MapRoute(
+			  name: "CheckLogin",
+			  url: "CheckLogin",
+			  defaults: new { controller = "Dashboard", action = "GetInfoUserLogin", id = UrlParameter.Optional }
+								).DataTokens = new RouteValueDictionary(new { area = "DashboardArea" });
 
-            routes.MapRoute(
-                name: "baocaopublic",
-                url: "baocaopublic",
-                defaults: new { controller = "baocaothongkeioc", action = "thongkepublic", id = UrlParameter.Optional }
-                                ).DataTokens = new RouteValueDictionary(new { area = "baocaothongkearea" });
+			routes.MapRoute(
+			   name: "AdminRoute",
+			   url: "admin",
+			   defaults: new { controller = "Dashboard", action = "Index", id = UrlParameter.Optional }
+								).DataTokens = new RouteValueDictionary(new { area = "DashboardArea" });
 
-            routes.MapRoute(
-               name: "AdminRoute",
-               url: "admin",
-               defaults: new { controller = "Dashboard", action = "Index", id = UrlParameter.Optional }
-                                ).DataTokens = new RouteValueDictionary(new { area = "DashboardArea" });
+			routes.MapRoute(
+							 name: "Default",
+							 url: "{controller}/{action}",
+							 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+							);
 
-            routes.MapRoute(
-                     name: "Default",
-                     url: "{controller}/{action}/{id}",
-                     defaults: new { controller = "AccountAdmin", action = "Login", id = UrlParameter.Optional }
-                    );
-
-            routes.MapRoute(
-                name: "DefaultArea",
-                url: "{area}/{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
-                );
-        }
-    }
+			routes.MapRoute(
+				name: "DefaultArea",
+				url: "{area}/{controller}/{action}/{id}",
+				defaults: new { action = "Index", id = UrlParameter.Optional }
+				);
+		}
+	}
 }
